e9011f4ecbffd8d5f8610138edc84cc3ec3f6030
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-12-20 03:04:35 -0500

    tailwind and shadcn

---
+++ components/HeaderBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <header class="sticky z-40 top-0 backdrop-blur-lg border-b border-border flex justify-between items-center h-14 pr-4 pl-4">
+        <a href="/">PairGrid</a>
+        <div>
+            <a href="/login">
+                <Button variant="outline">
+                    Login
+                </Button>
+            </a>
+        </div>
+    </header>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,4 +5,5 @@ export default defineNuxtConfig({
     // prerender index route by default
     '/': { prerender: true },
   },
+  modules:['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
 });
