e38e46976dd0a006fdbba73a534117ef6d95d17e
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-12-20 19:04:04 -0500

    clerk nuxt version

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
+<script setup lang="ts"></script>
 <template>
-  <HeaderBar />
+  <header class="sticky z-40 top-0 backdrop-blur-lg border-b border-border flex justify-between items-center h-14 pr-4 pl-4">
+      <NuxtLink to="/">PairGrid</NuxtLink>
+      <div>
+          <SignedOut>
+              <SignInButton />
+          </SignedOut>
+          <SignedIn>
+              <UserButton />
+          </SignedIn>
+      </div>
+  </header>
   <NuxtPage />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   routeRules: {
     // prerender index route by default
     '/': { prerender: true },
   },
-  modules:['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+
+  modules:['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@clerk/nuxt'],
+  compatibilityDate: '2024-12-20',
 });
