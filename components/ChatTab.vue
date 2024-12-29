7efbbf63e660a73c569de663769b2664c68a4feb
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-12-28 19:55:41 -0500

    resolve import

--- components/ChatTab.vue
+++ components/ChatTab.vue
@@ -147,6 +147,7 @@
   import { ref, defineProps, defineEmits, onMounted, onBeforeUnmount } from 'vue'
   import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
   import Pusher from 'pusher-js'
+  import { useRuntimeConfig } from '#app'
   import CryptoJS from 'crypto-js'
 
   const props = defineProps({
