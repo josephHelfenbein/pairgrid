0b7ea50534a58d10eac734f550e88dd10934d4a0
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-12-30 19:48:29 -0500

    test changing chat area

--- components/ChatArea.vue
+++ components/ChatArea.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col h-[calc(100vh-350px)]">
+    <div class="flex flex-col h-full">
       <ScrollArea ref="scrollArea" class="flex-grow mb-4">
         <div class="flex justify-center items-center h-full" v-if="chatLoading">
           <Loader size="80px" />
