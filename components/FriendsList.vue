cafac4a53461c040a060c54d591ef4426aac4693
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-12-31 21:15:48 -0500

    test wrapping dialogtrigger in button

--- components/FriendsList.vue
+++ components/FriendsList.vue
@@ -12,11 +12,13 @@
         >
           <div class="flex items-center gap-2">
             <img :src="request.profile_picture" class="w-8 h-8 rounded-full object-cover" />
-            <DialogTrigger @click="$emit('fetchRequestProfile', request)" role="button" asChild>
-            <p class="cursor-pointer">
-              {{ request.name }}
-            </p>
-            </DialogTrigger>
+            <button @click="$emit('fetchRequestProfile', request)">
+              <DialogTrigger asChild>
+              <p>
+                {{ request.name }}
+              </p>
+              </DialogTrigger>
+            </button>
           </div>
           <div class="flex gap-2">
             <button @click="$emit('acceptRequest', request)" class="p-2 bg-green-500 text-white rounded-full">
