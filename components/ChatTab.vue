a36b787c893820a8b4864f114a7f5167957c8665
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-01-05 22:43:00 -0500

    fix notifications fetching

--- components/ChatTab.vue
+++ components/ChatTab.vue
@@ -173,7 +173,6 @@
   import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
   import { ScrollArea } from '@/components/ui/scroll-area'
   import { useRuntimeConfig } from '#app'
-  import CryptoJS from 'crypto-js'
   import Pusher from 'pusher-js'
   import { useSession } from '@clerk/vue'
 
@@ -242,7 +241,7 @@
       })
       if (!response.ok) throw new Error('Failed to fetch notifications')
       const data = await response.json();
-      notifications.value = JSON.parse(JSON.stringify(data));
+      if(data.length > 0) notifications.value = JSON.parse(JSON.stringify(data));
     } catch (err) {
       console.error(err)
       emit('toast-update', 'Error fetching notifications')
