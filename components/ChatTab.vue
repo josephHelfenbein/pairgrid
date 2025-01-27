18c9315001cf97a9adcdac2b2fcd03bc33da975e
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-01-27 17:28:32 -0500

    test adding support for multiple device sending

--- components/ChatTab.vue
+++ components/ChatTab.vue
@@ -489,6 +489,13 @@
         setTimeout(fetch(`https://www.pairgrid.com/api/getmessages/getmessages?user_id=${props.user.id}&friend_id=${selectedFriend.value.id}&notification_stopper=true`, {
           method: 'GET',
         }), 2000)
+      } else if(data.sender_id == props.user.id && !messages.value.includes(data.created_at)) {
+        messages.value.push({
+          id: data.created_at,
+          sender: data.sender_id == props.user.id ? props.user.fullName : selectedFriend.value.name,
+          senderIcon: data.sender_id == props.user.id ? props.preferences.profilePicture : selectedFriend.value.profile_picture,
+          text: data.encrypted_content,
+        })
       }
     })
     pusher.value.connection.bind('error', (err) => {
